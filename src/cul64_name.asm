a898ee64b0a4e6b222dfe5289c056f9a6bb4210d
Russell Bulmer <russell.bulmer@gmail.com>
2026-05-17 15:53:40 +0100

    basic single name generation for system diplomats

--- src/cul64_name.asm
+++ src/cul64_name.asm
@@ -4,31 +4,55 @@ NAME_GENERATE
     ; TODO style? one long, two med, three small, med-single-med, etc
 
     ; test with max length 
-    lda #BB_MAX_CHARS
+    lda #BB_MAX_CHARS/2         ; specifies number of pairs, max 8
     sta NAME_LEN
     jsr NAME_GEN_MAX_LEN
 
     rts 
 
+; generates NAME_LEN pairs
 NAME_GEN_LEN
-    ldx #0
+    ldy #0                      ; loop counter
+    ldx #0                      ; name buf index
+
 .name_gen_len_loop
-    cpx NAME_LEN
+    cpy NAME_LEN
     beq .name_gen_len_done
 
-; hacky hack hack
-    lda 'q'
+    tya
+    pha                         ; save loop counter
 
+    ; first char of pair
+    lda LFSR_W0
+    and #%01111111              ; 0-127
+    asl                         ; shift left, multiple by 2 = 0-254 index
+    tay                         
+    lda NAME_THE_ONE_STRING, y  ; first char of pair
     sta NAME_BUFFER, x
-    inx
+    inx                         ; inc buf index 
+    iny                         ; one string index
+
+    ; second char of pair
+    lda NAME_THE_ONE_STRING, y  ; first char of pair
+    cmp #$20                    ; check for space
+    beq +                       ; skip space
+    sta NAME_BUFFER, x          ; second char
+    inx                         ; inc buf index 
++
+    pla                         ; restore loop counter
+    tay
+    iny                         ; inc loop counter
+    jsr LFSR_NEXT_SEED          ; wasteful as we only used w0 but easy...
     jmp .name_gen_len_loop
+
 .name_gen_len_done:
    lda #0              ; null terminator
     sta NAME_BUFFER, x
     rts 
 
+; max num of pairs
 NAME_GEN_MAX_LEN        
-    dec NAME_LEN        ; make 0 to max-1, eg len is 10, so make 0-9
+    dec NAME_LEN        ; make 0 to max-1, eg len is 6, so make 0-5
     lda NAME_LEN
     and LFSR_W0         ; simple and randomly maxes it - cute trick!
     sta NAME_LEN
@@ -38,13 +62,14 @@ NAME_GEN_MAX_LEN
 
 NAME_BUFFER
     !fill BB_MAX_CHARS+1, 0
+NAME_LEN
+    !byte 0
+
 NAME_THE_ONE_STRING     ; 128 pairs of chars
     !scr "a i u e o n kakikukekokysasisusesosytasitutetotynaninunenonyhahihuhehohymamimumemomyyayiyuyeyoy "     ; 96
     !scr "rarirurerorywawiwuwewowygagigugegogyzazizuzezozydadidudedodybabibubebobypapipupepopyjajijujejojykakikukekokya "   ; 110
-    !scr "i u e o n chstthckprlalelilolytrqu/ ' - . : + "   ; 46
+    !scr "qucldrbrtrchstthfafifofulaleliloly/ ' - . : + "   ; 46
     !byte 73, $20, 75, $20      ; 4
-NAME_LEN
-    !byte 0
 
 
 
