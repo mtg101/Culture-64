1b5efcd588e991708e81ede1127201bbbd3191b4
Russell Bulmer <russell.bulmer@gmail.com>
2026-05-31 18:09:43 +0100

    kinda got plasma clouds working but... linear not x/y

--- src/cul64_nebula_clouds.asm
+++ src/cul64_nebula_clouds.asm
@@ -3,18 +3,18 @@
 CLOUDS_PATCH_FONT:
     ; --- Setup Pointers in Zero Page ---
     ; CLOUDS_UDG_BASE 196*8=1568/$620
-    lda #20
-    sta ZP_PTR_2            ; Destination Low ($00 of $3620)
-    lda #$36                ; Destination High ($32 of $3620)
+    lda #$20
+    sta ZP_PTR_2            ; Destination Low ($20 of $3620)
+    lda #$36                ; Destination High ($36 of $3620)
     sta ZP_PTR_2_PAIR
 
-    ldy #$00                ; Clear Y index
+    ldy #0                  ; Clear Y index
     
 -
-    lda CLOUDS_UDGS,y       ; Grab byte from ROM
-    sta (ZP_PTR_2),y        ; Write byte to RAM
+    lda CLOUDS_UDGS,y       ; Grab byte from UDG
+    sta (ZP_PTR_2),y        ; Write byte to font
     iny                     ; Next byte
-    cpy #64                 ; 8 chars 64 bytes
+    cpy #32                 ; 4 chars 32 bytes
     bne -
     rts 
 
@@ -89,13 +89,11 @@ CLOUDS_SHOW:
     lsr 
     lsr 
 
-    ; 0-11 not visible
+    ; only 0-3 show
     sec 
-    cmp #12
-    bcc +                       ; skip 0-12 and leave stars / space behind
+    cmp #3
+    bcs +                
 
-    ; draw based on val 12-15
-    and #%00000011              ; 0-3
     clc
     adc #CLOUDS_UDG_BASE
     sta TEXT_CHAR
@@ -151,48 +149,43 @@ CLOUDS_STEP_2
 CLOUDS_STEP_3
     !byte 0
 
-; Char 0: Completely Black / Empty
-CLOUDS_UDGS
-; Char 1: Very Sparse Stipple (12.5% density) 
+CLOUDS_UDGS         ; dense to light
 CLOUDS_1
-    !byte %00000000
-    !byte %00100000
-    !byte %00000000
-    !byte %00000100
-    !byte %00100000
-    !byte %00000000
-    !byte %00000000
-    !byte %00000100
-; Char 2: Light Checkerboard / Ordered Dither (25% density)
-CLOUDS_2
-    !byte %01000000
-    !byte %00001000
-    !byte %00000000
-    !byte %00100010
-    !byte %00001000
-    !byte %01000000
-    !byte %00010000
-    !byte %00000010
-; Char 3: Sparse Interlaced Dither (37.5% density)
-CLOUDS_3
     !byte %01000100
-    !byte %00000010
-    !byte %00100000
-    !byte %01000010
-    !byte %00000100
-    !byte %00101000
-    !byte %00100000
-    !byte %00001001
-; Char 4: Perfect 50% Checkerboard Dither
-CLOUDS_4
-    !byte %00001010
-    !byte %00110000
-    !byte %10000010
-    !byte %01000010
+    !byte %00010000
+    !byte %01000100
     !byte %00100010
     !byte %01001000
+    !byte %01000010
     !byte %00010000
-    !byte %01000100
+    !byte %00100100
+CLOUDS_2
+    !byte %01000000
+    !byte %00000100
+    !byte %10010000
+    !byte %01000000
+    !byte %00001000
+    !byte %01000000
+    !byte %00010010
+    !byte %00001000
+CLOUDS_3
+    !byte %00000000
+    !byte %00100000
+    !byte %00000010
+    !byte %00010000
+    !byte %00000000
+    !byte %01000000
+    !byte %00001000
+    !byte %00000000
+CLOUDS_4
+    !byte %00000000
+    !byte %00100000
+    !byte %00000000
+    !byte %00000000
+    !byte %00000100
+    !byte %00000000
+    !byte %01000000
+    !byte %00000000
 
 
 ; =====================================================================
@@ -208,7 +201,7 @@ CLOUDS_SINE_LUT
     !byte $4c,$4d,$4d,$4d,$4e,$4e,$4e,$4f,$4f,$4f,$4f,$4f,$4f,$4f,$4f,$4f
     !byte $50,$4f,$4f,$4f,$4f,$4f,$4f,$4f,$4f,$4f,$4e,$4e,$4e,$4d,$4d,$4d
     !byte $4c,$4c,$4c,$4b,$4b,$4a,$4a,$49,$49,$48,$48,$47,$46,$46,$45,$44
-    !byte $44,$43,$42,$42,$41,$40,$3f,$3f,$e,$3d,$3c,$3b,$3a,$39,$39,$38
+    !byte $44,$43,$42,$42,$41,$40,$3f,$3f,$3e,$3d,$3c,$3b,$3a,$39,$39,$38
     !byte $37,$36,$35,$34,$33,$32,$31,$30,$2f,$2e,$2d,$2c,$2b,$2a,$29,$28
     !byte $28,$27,$26,$25,$24,$23,$22,$21,$20,$1f,$1e,$1d,$1c,$1b,$1a,$19
     !byte $18,$17,$16,$16,$15,$14,$13,$12,$11,$10,$10,$0f,$0e,$0d,$0d,$0c
